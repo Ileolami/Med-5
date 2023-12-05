38eeebd917d9cae3a159463b104c03aac359e52f
ileolami <makindemercymiracle@gmail.com>
2023-12-05 12:59:02 +0100

    update

---
+++ src/components/login.jsx
@@ -0,0 +1,14 @@
+const Login = () => {
+    return(
+        <div>
+            <form action="">
+                <input type="text" name="username" id="username" placeholder="username"/>
+                <input type="password" name="password" id="password" placeholder="password"/>
+                <input type="submit" value="submit"/>
+            </form>
+        </div>
+    )
+}
+
+
+export default Login;
