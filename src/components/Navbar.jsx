8548e10d868c2d6df559e9c59fa69f57b166173e
Ileolami <99335756+Ileolami@users.noreply.github.com>
2023-12-16 14:12:20 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,11 +19,7 @@ const Navbar = () => {
       <header className="container mx-auto flex items-center justify-between p-4 font-Playfair text-white" data-aos="fade-down" data-aos-delay="500">
         <div className="flex items-center gap-5">
           <img src={Logo} alt="med5logo" /> 
-          <p className='text-hover italic text-xs p-2 border-orange-800 border-2 border-solid '>
-            experimental
-          </p>
-        </div>
-       
+        
         <nav
           className={`md:flex ${
             isMenuOpen ? 'block' : 'hidden'
